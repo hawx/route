6ab92051127dea2bde2408171567d359d4a90d7b
Joshua Hawxwell <m@hawx.me>
2015-03-18 15:13:45 +0000

    Remove redirect options, always set true; add Default router

--- router.go
+++ router.go
@@ -87,24 +87,6 @@ import (
 type Router struct {
 	tree *node
 
-	// Enables automatic redirection if the current route can't be matched but a
-	// handler for the path with (without) the trailing slash exists.
-	// For example if /foo/ is requested but a route only exists for /foo, the
-	// client is redirected to /foo with http status code 301 for GET requests
-	// and 307 for all other request methods.
-	RedirectTrailingSlash bool
-
-	// If enabled, the router tries to fix the current request path, if no
-	// handle is registered for it.
-	// First superfluous path elements like ../ or // are removed.
-	// Afterwards the router does a case-insensitive lookup of the cleaned path.
-	// If a handle can be found for this route, the router makes a redirection
-	// to the corrected path with status code 301 for GET requests and 307 for
-	// all other request methods.
-	// For example /FOO and /..//Foo could be redirected to /foo.
-	// RedirectTrailingSlash is independent of this option.
-	RedirectFixedPath bool
-
 	// Configurable http.HandlerFunc which is called when no matching route is
 	// found. If it is not set, http.NotFound is used.
 	NotFound http.HandlerFunc
@@ -117,16 +99,22 @@ type Router struct {
 	PanicHandler func(http.ResponseWriter, *http.Request, interface{})
 }
 
+var Default = New()
+
+func Handle(path string, handler http.Handler) {
+	Default.Handle(path, handler)
+}
+
+func HandleFunc(path string, handler http.HandlerFunc) {
+	Default.HandleFunc(path, handler)
+}
+
 // Make sure the Router conforms with the http.Handler interface
 var _ http.Handler = New()
 
 // New returns a new initialized Router.
-// Path auto-correction, including trailing slashes, is enabled by default.
 func New() *Router {
-	return &Router{
-		RedirectTrailingSlash: true,
-		RedirectFixedPath:     true,
-	}
+	return &Router{}
 }
 
 // Handler registers a new request handle with the given path.
@@ -182,7 +170,7 @@ func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 				code = 307
 			}
 
-			if tsr && r.RedirectTrailingSlash {
+			if tsr {
 				if len(path) > 1 && path[len(path)-1] == '/' {
 					req.URL.Path = path[:len(path)-1]
 				} else {
@@ -193,16 +181,14 @@ func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 			}
 
 			// Try to fix the request path
-			if r.RedirectFixedPath {
-				fixedPath, found := root.findCaseInsensitivePath(
-					cleanPath(path),
-					r.RedirectTrailingSlash,
-				)
-				if found {
-					req.URL.Path = string(fixedPath)
-					http.Redirect(w, req, req.URL.String(), code)
-					return
-				}
+			fixedPath, found := root.findCaseInsensitivePath(
+				cleanPath(path),
+				true,
+			)
+			if found {
+				req.URL.Path = string(fixedPath)
+				http.Redirect(w, req, req.URL.String(), code)
+				return
 			}
 		}
 	}
